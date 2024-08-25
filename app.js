3eadf761fb67d407a02236e89582d82196a63ce8
Krishna Kaushal <104532938+tsu-ki@users.noreply.github.com>
2024-08-25 06:59:49 +0530

    app.js

--- app.js
+++ app.js
@@ -13,8 +13,6 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 })
     .then(() =>console.log('Connected to MongoDB'))
     .catch((err) => console.log('Error connecting to MongoDB:', err));
